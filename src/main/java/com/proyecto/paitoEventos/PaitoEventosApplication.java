c9d4c510c0619c73112656c337fb48eb57202d5f
Diego Armando <160793240+DiegoArmandoCayetano@users.noreply.github.com>
2026-06-10 22:32:40 -0500

    Subir el archivo descargable de https://start.spring.io/ con las dependencias para el software web, el archivo base del proyecto

---
+++ src/main/java/com/proyecto/paitoEventos/PaitoEventosApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.paitoEventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaitoEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaitoEventosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/paitoEventos/PaitoEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.paitoEventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaitoEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
