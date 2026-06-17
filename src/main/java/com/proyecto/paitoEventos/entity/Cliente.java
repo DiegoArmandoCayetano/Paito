b712a759c42b4ccc24fb18591865d43b0f6b3f5d
Diego Armando <160793240+DiegoArmandoCayetano@users.noreply.github.com>
2026-06-16 23:08:42 -0500

    crear archivo Cliente.java en el paquete entity

---
+++ src/main/java/com/proyecto/paitoEventos/entity/Cliente.java
@@ -0,0 +1,32 @@
+package com.proyecto.paitoEventos.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDate;
+
+@Entity
+@Table(name= "clientes")
+@Getter
+@Setter
+
+public class Cliente {
+    
+    @Id
+    @Column(name = "id_usuario")
+    private Integer id_usuario;
+
+    @Column(nullable=false, length=100)
+    private String ciudad;
+
+    @Column(nullable=false, length=100)
+    private String direccion;
+
+    @Column(name="fecha_creacion", nullable=false)
+    private LocalDate fechaCreacion;
+
+    @OneToOne
+    @MapsId//Esto le dice al JPA que el id_usuario de la entidad Uusario como PK y FK al mismo tiempo
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+}
