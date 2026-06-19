2d9f285b805f35771d838625d10d288ee4cb2245
Diego Armando <160793240+DiegoArmandoCayetano@users.noreply.github.com>
2026-06-18 22:23:34 -0500

    crear archivo Calificacion.java en el paquete entity

---
+++ src/main/java/com/proyecto/paitoEventos/entity/Calificacion.java
@@ -0,0 +1,35 @@
+package com.proyecto.paitoEventos.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "calificaciones")
+@Getter
+@Setter
+public class Calificacion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_calificacion")
+    private Integer idCalificacion;
+
+    @Column(nullable = false)
+    private Byte puntuacion; // TINYINT en MySQL mapea excelente a Byte en Java
+
+    @Column(columnDefinition = "TEXT")
+    private String comentario;
+
+    @Column(nullable = false)
+    private LocalDate fecha;
+
+    @ManyToOne
+    @JoinColumn(name = "id_cliente", nullable = false)
+    private Cliente cliente;
+
+    @ManyToOne
+    @JoinColumn(name = "id_artista", nullable = false)
+    private Artista artista;
+}
