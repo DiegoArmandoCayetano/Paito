ab8ade1521346e01751a3f467353f0444f5f8d04
Diego Armando <160793240+DiegoArmandoCayetano@users.noreply.github.com>
2026-07-01 11:50:00 -0500

    desarrollar el archivo SolicitudController.java en la carpeta controller

--- src/main/java/com/proyecto/paitoEventos/controller/SolicitudController.java
+++ src/main/java/com/proyecto/paitoEventos/controller/SolicitudController.java
@@ -1,9 +1,34 @@
 package com.proyecto.paitoEventos.controller;
 
+import com.proyecto.paitoEventos.service.SolicitudService;
+import com.proyecto.paitoEventos.entity.Solicitud;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+
+@RestController
+@RequestMapping("/api/solicitudes")
+@CrossOrigin(origins="*")
 
 public class SolicitudController {
     
+    @Autowired
+    private SolicitudService solicitudService;
     
+    @PostMapping("/cliente/{idCliente}")
+
+    public ResponseEntity<?> crearSolicitud(@RequestBody Solicitud solicitud,
+        @PathVariable Integer idCliente) {
+        
+        try{
+            Solicitud nuevaSolicitud = solicitudService.crearSolicitud(solicitud, idCliente);
+            return ResponseEntity.status(HttpStatus.CREATED).body(nuevaSolicitud);
+        }catch(RuntimeException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+
+    }
 
 }
