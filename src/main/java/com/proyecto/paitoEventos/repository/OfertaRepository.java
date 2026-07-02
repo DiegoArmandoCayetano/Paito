1396e25e6754bafc9bb970bd7ca3fb27e1b8212c
Diego Armando <160793240+DiegoArmandoCayetano@users.noreply.github.com>
2026-07-02 11:13:19 -0500

    incluir metodo aceptarOferta en la clase OfertaService.java y añadir metodo findBySolicitudIdSolicitud de la clase OfertaRepository para que me de una lista de todas las ofertas que tienen una solicitud

--- src/main/java/com/proyecto/paitoEventos/repository/OfertaRepository.java
+++ src/main/java/com/proyecto/paitoEventos/repository/OfertaRepository.java
@@ -2,6 +2,16 @@ package com.proyecto.paitoEventos.repository;
 import com.proyecto.paitoEventos.entity.Oferta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
-public interface OfertaRepository extends JpaRepository<Oferta, Integer> {}
+public interface OfertaRepository extends JpaRepository<Oferta, Integer> {
+
+    // Método nuevo para buscar por el ID de la solicitud vinculada
+
+    //Busca y extrae de la base de datos MySQL un listado con todas las ofertas económicas
+    //  que existan en el sistema cuyo evento o solicitud coincida exactamente 
+    // con el número de ID que le indiques por parámetro.
+    List<Oferta> findBySolicitudIdSolicitud(Integer idSolicitud);
+
+}
--- src/main/java/com/proyecto/paitoEventos/service/OfertaService.java
+++ src/main/java/com/proyecto/paitoEventos/service/OfertaService.java
@@ -2,6 +2,7 @@ package com.proyecto.paitoEventos.service;
 
 import com.proyecto.paitoEventos.entity.Artista;
 import com.proyecto.paitoEventos.entity.EstadoOferta;
+import com.proyecto.paitoEventos.entity.EstadoSolicitud;
 import com.proyecto.paitoEventos.entity.Oferta;
 import com.proyecto.paitoEventos.entity.Solicitud;
 import com.proyecto.paitoEventos.repository.ArtistaRepository;
@@ -11,6 +12,7 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 
@@ -53,4 +55,55 @@ public class OfertaService {
 
         return ofertaRepository.save(oferta);
     }
+
+    @Transactional
+    public Oferta aceptarOferta(Integer idOferta){
+
+        if(!ofertaRepository.existsById(idOferta)){
+            throw new RuntimeException("La oferta no existe");
+        }
+
+         //Aprobar la oferta elegida
+        Oferta ofertaElegida = ofertaRepository.findById(idOferta).get();
+        
+
+        if(ofertaElegida.getEstadoOferta() == EstadoOferta.ACEPTADA ||
+           ofertaElegida.getEstadoOferta() == EstadoOferta.RECHAZADA){
+            throw new RuntimeException("no se puede aceptar una oferta ya procesada");
+        }
+
+        //Cerrar la solicitud del evento:
+        Solicitud solicitudVinculada = ofertaElegida.getSolicitud();
+        
+        if(solicitudVinculada.getEstadoSolicitud() ==  EstadoSolicitud.ACEPTADA ||
+           solicitudVinculada.getEstadoSolicitud() ==  EstadoSolicitud.FINALIZADA){
+            throw new RuntimeException("Este evento ya cerró contrato con otro artista");
+        }      
+
+        //CAMBIOS DE ESTADO (Aprobar oferta y cerrar solicitud)
+        ofertaElegida.setEstadoOferta(EstadoOferta.ACEPTADA);
+        solicitudVinculada.setEstadoSolicitud(EstadoSolicitud.ACEPTADA);
+
+        // ==========================================
+        // NUEVA LÓGICA: RECHAZAR LA COMPETENCIA
+        // ==========================================
+
+        //Traemos TODAS las ofertas asociadas a esta misma solicitud
+        List<Oferta> todasLasOfertas = ofertaRepository.findBySolicitudIdSolicitud(solicitudVinculada.getIdSolicitud());
+
+        // Recorremos la lista con un bucle para alterar a las perdedoras
+        for(Oferta operacion : todasLasOfertas){
+            if(!operacion.getIdOferta().equals(ofertaElegida.getIdOferta())){
+                operacion.setEstadoOferta(EstadoOferta.RECHAZADA);
+                
+                // Guardamos el rechazo en MySQL
+                ofertaRepository.save(operacion);
+
+            }
+        }
+
+        solicitudRepository.save(solicitudVinculada);
+        return ofertaRepository.save(ofertaElegida);
+    }
+
 }
